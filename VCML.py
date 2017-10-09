c497b0f9d91ab9dd81e0ddbcf37cfb5aa451f824
hoenn321 <henryemerald321@yahoo.ca>
2017-10-08 20:28:21 -0400

    Updated cross_validation to model_selection
    Used model_selection instead due to cross_validation deprecation warning.

--- VCML.py
+++ VCML.py
@@ -1,7 +1,7 @@
 import csv , time
 import numpy as np
 # import tensorflow as tf
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
